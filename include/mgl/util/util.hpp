bfc86409139e773e7707561d2ebc681cc6d30453
ethan hofton <ethanhofton@gmail.com>
2022-05-20 20:20:11 +0100

    commit

--- include/mgl/util/util.hpp
+++ include/mgl/util/util.hpp
@@ -21,7 +21,7 @@ using namespace rapidjson;
 // * used to deside weather to include the imgui code into the final build
 // * to compile with IMGUI, use -DUSE_IMGUI
 // * wrap all IMGUI code statements with IMGUI()
-#if USE_IMGUI == 1
+#if (USE_IMGUI == 1)
 #define IMGUI(...) __VA_ARGS__
 #else
 #define IMGUI(...)
