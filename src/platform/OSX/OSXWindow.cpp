3ad5127b7a4cc1996c0b261bd4cef8655ec4c874
ethan hofton <ethanhofton@gmail.com>
2022-05-24 14:59:20 +0100

    testing

--- src/platform/OSX/OSXWindow.cpp
+++ src/platform/OSX/OSXWindow.cpp
@@ -27,6 +27,10 @@ namespace mgl
     OSXWindow::OSXWindow(const windowProps& t_props)
     {
         init(t_props);
+
+        int w, h;
+        glfwGetWindowSize(m_window, &w, &h);
+        MGL_CORE_INFO("Window Size: ({}, {})", w, h);
     }
 
     OSXWindow::~OSXWindow()
